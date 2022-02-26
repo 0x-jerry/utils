66f11a305de6d3504633299256b32d98241e06bb
Jerry Wang <x.jerry.wang@gmail.com>
2022-02-26 18:52:20 +0800

    test: test edge condition for sleep

--- src/core/sleep.test.ts
+++ src/core/sleep.test.ts
@@ -4,6 +4,7 @@ describe('sleep', () => {
   it('wait 200 ms', (done) => {
     let v = 0
     sleep(200).then(() => (v = 1))
+    expect(v).toBe(0)
 
     setTimeout(() => {
       expect(v).toBe(1)
