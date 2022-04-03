73edf8c6b38b6f43a9e90c387fdd733728ebc851
Jerry Wang <x.jerry.wang@gmail.com>
2022-04-03 12:40:46 +0800

    docs: write comment for isClass

--- src/core/is.ts
+++ src/core/is.ts
@@ -1,5 +1,13 @@
 import { Ctor } from './types'
 
+/**
+ * Please attention, this is only working with ES6.
+ *
+ * It will not working if you compile code to ES5.
+ *
+ * @param o
+ * @returns
+ */
 export function isClass<Ins, Params>(o: unknown): o is Ctor<Ins, Params> {
   return /^\s*class/.test(String(o))
 }
