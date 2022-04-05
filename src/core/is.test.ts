ec435b45f361435f4ea23eb638b93715f8eead59
Jerry Wang <x.jerry.wang@gmail.com>
2022-04-05 21:55:54 +0800

    test: add unit test for is utils

--- src/core/is.test.ts
+++ src/core/is.test.ts
@@ -8,4 +8,45 @@ describe('is utils', () => {
 
     expect(is.classs(function () {})).toBe(false)
   })
+
+  it('is number', () => {
+    expect(is.number(1)).toBe(true)
+
+    expect(is.number(0o1)).toBe(true)
+    expect(is.number(0x1)).toBe(true)
+
+    expect(is.number('123')).toBe(false)
+  })
+
+  it('is string', () => {
+    expect(is.string('')).toBe(true)
+
+    expect(is.string(1)).toBe(false)
+
+    expect(is.string({})).toBe(false)
+  })
+
+  it('is boolean', () => {
+    expect(is.boolean(false)).toBe(true)
+
+    expect(is.boolean(1)).toBe(false)
+    expect(is.boolean('1')).toBe(false)
+  })
+
+  it('is fn', () => {
+    expect(is.fn(() => {})).toBe(true)
+
+    expect(is.fn(async () => {})).toBe(true)
+
+    expect(is.fn(class A {})).toBe(false)
+    expect(is.fn(1)).toBe(false)
+    expect(is.fn('1')).toBe(false)
+  })
+
+  it('is array', () => {
+    expect(is.array([])).toBe(true)
+
+    expect(is.array(1)).toBe(false)
+    expect(is.array(new Set())).toBe(false)
+  })
 })
