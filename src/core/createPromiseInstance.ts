dc5bb18721f4b329c56285fffaeb376ff3bcdf29
0x-jerry <x.jerry.wang@gmail.com>
2022-04-19 11:36:21 +0800

    feat: add promise status

--- src/core/createPromiseInstance.ts
+++ src/core/createPromiseInstance.ts
@@ -1,7 +1,18 @@
-export interface PromiseInstance<T> {
+export interface PromiseInstance<T = any> {
+  readonly status: PromiseStatus
+  readonly isPending: boolean
+  readonly isFulfilled: boolean
+  readonly isRejected: boolean
+
   instance: Promise<T>
   resolve: (data: T | PromiseLike<T>) => void
   reject: (reason: any) => void
+}
+
+export enum PromiseStatus {
+  Pending = 'pending',
+  Fulfilled = 'fulfilled',
+  Rejected = 'rejected',
 }
 
 export function createPromiseInstance<T>(): PromiseInstance<T> {
@@ -10,12 +21,34 @@ export function createPromiseInstance<T>(): PromiseInstance<T> {
 
   let _resolve: Resolve, _reject: Reject
 
+  let _status = PromiseStatus.Pending
+
   const p = new Promise<T>((resolve, reject) => {
-    _resolve = resolve
-    _reject = reject
+    _resolve = (v) => {
+      _status = PromiseStatus.Fulfilled
+      resolve(v)
+    }
+
+    _reject = (r) => {
+      _status = PromiseStatus.Rejected
+
+      reject(r)
+    }
   })
 
   return {
+    get status() {
+      return _status
+    },
+    get isPending() {
+      return _status === PromiseStatus.Pending
+    },
+    get isFulfilled() {
+      return _status === PromiseStatus.Fulfilled
+    },
+    get isRejected() {
+      return _status === PromiseStatus.Rejected
+    },
     instance: p,
     resolve: _resolve!,
     reject: _reject!,
