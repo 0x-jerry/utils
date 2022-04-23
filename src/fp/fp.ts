47894883acaee9c8b3f4244cfcfe303095319047
Jerry Wang <x.jerry.wang@gmail.com>
2022-04-23 14:59:49 +0800

    feat: pipe style for fp

--- src/fp/fp.ts
+++ src/fp/fp.ts
@@ -24,3 +24,39 @@ export const compose: ICompose = (...fns: any[]) => {
 
   return composed as any
 }
+
+/**
+ *
+ * like {@link compose}, but use `pipe` style
+ *
+ * @example
+ *
+ * ```ts
+ * const plusOne = (n: number) => n + 1
+ * const toString = (n: number) => n.toString()
+ *
+ * input(0).pipe(plusOne).pipe(toString).exec() // '1'
+ * ```
+ *
+ * @param input
+ * @returns
+ */
+export const input = <I>(input: I) => new Pipeline<I, I>((i) => i, input)
+
+class Pipeline<Output, Input> {
+  #fn: Fn<Output, [Input]>
+  #input: Input
+
+  constructor(fn: Fn<Output, [Input]>, input: Input) {
+    this.#fn = fn
+    this.#input = input
+  }
+
+  pipe<NextOutput>(fn: Fn<NextOutput, [Output]>) {
+    return new Pipeline(compose(this.#fn, fn), this.#input)
+  }
+
+  exec() {
+    return this.#fn(this.#input)
+  }
+}
--- src/fp/fs.test.ts
+++ src/fp/fs.test.ts
@@ -1,13 +1,19 @@
-import { compose } from './fp'
+import { compose, input } from './fp'
 
 describe('function program', () => {
-  it('should compose two function into one', () => {
-    const plusOne = (n: number) => n + 1
-    const toString = (n: number) => n.toString()
+  const plusOne = (n: number) => n + 1
+  const toString = (n: number) => n.toString()
 
+  it('should compose two function into one', () => {
     const plusOneThenToString = compose(plusOne, toString)
 
     expect(plusOneThenToString(0)).toBe('1')
     expect(plusOneThenToString.exec(0)).toBe('1')
+  })
+
+  it('use pipe style', () => {
+    const x = input(0).pipe(plusOne).pipe(toString).exec()
+
+    expect(x).toBe('1')
   })
 })
