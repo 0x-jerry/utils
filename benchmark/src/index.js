ab8515d2bfb2f6dd99dfc05038507eacfacfa4bf
0x-jerry <x.jerry.wang@gmail.com>
2022-04-27 10:58:27 +0800

    perf: expose nanoid as uuid

---
+++ benchmark/src/index.js
@@ -0,0 +1,22 @@
+const Benchmark = require('benchmark')
+const {} = require('@0x-jerry/utils')
+
+const suite = new Benchmark.Suite()
+
+// add tests
+suite
+  .add('case#1', function () {
+    //
+  })
+  .add('case#2', function () {
+    //
+  })
+  // add listeners
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  // run async
+  .run({ async: true })
--- src/core/misc.ts
+++ src/core/misc.ts
@@ -1,4 +1,7 @@
 /* c8 ignore next */
+export { nanoid as uuid } from 'nanoid'
+
+/* c8 ignore next */
 export const noop = () => {}
 
 export const hasOwn = (o: Object, name: string) => Object.prototype.hasOwnProperty.call(o, name)
