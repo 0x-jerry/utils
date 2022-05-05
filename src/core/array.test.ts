80fd463a318b4258bf39de340c1ec9879d233b25
0x-jerry <x.jerry.wang@gmail.com>
2022-05-05 16:53:29 +0800

    feat: add remove function for array

--- src/core/array.test.ts
+++ src/core/array.test.ts
@@ -1,4 +1,4 @@
-import { toArray } from './array'
+import { remove, toArray } from './array'
 
 describe('toArray', () => {
   it('array', () => {
@@ -13,5 +13,24 @@ describe('toArray', () => {
 
   it('2 dimension', () => {
     expect(toArray([[1], [2]])).toEqual([[1], [2]])
+  })
+})
+
+describe('remove', () => {
+  it('should remove exists item', () => {
+    const item = [1, 2, { a: 1 }] as any[]
+
+    expect(remove(item, 2)).toBe(1)
+    expect(item).toEqual([1, { a: 1 }])
+    expect(remove(item, (o) => o.a === 1)).toBe(1)
+    expect(item).toEqual([1])
+  })
+
+  it('should return -1', () => {
+    const item = [1, 2]
+
+    expect(remove(item, 3)).toBe(-1)
+    expect(remove(item, (o) => o === 3)).toBe(-1)
+    expect(item).toEqual([1, 2])
   })
 })
--- src/core/array.ts
+++ src/core/array.ts
@@ -1,3 +1,4 @@
+import { is } from '../is'
 import { Arrayable } from './types'
 
 /**
@@ -8,4 +9,12 @@ import { Arrayable } from './types'
  */
 export const toArray = <T>(o: Arrayable<T>): T[] => {
   return Array.isArray(o) ? o : [o]
+}
+
+export const remove = <T>(o: T[], predict: T | ((item: T) => boolean)): number => {
+  const idx = is.fn(predict) ? o.findIndex(predict) : o.indexOf(predict)
+
+  if (idx >= 0) o.splice(idx, 1)
+
+  return idx
 }
