9df7cc052a4014ee94eeacf888615af163f549ec
Jerry Wang <x.jerry.wang@gmail.com>
2022-09-27 14:02:26 +0800

    docs: add example for createSingleton, skip ci

--- src/core/createSingleton.ts
+++ src/core/createSingleton.ts
@@ -3,6 +3,23 @@ import { Ctor } from './types'
 
 const singletons = new WeakMap<Function, unknown>()
 
+/**
+ *
+ * @example
+ *
+ * ```ts
+ * class Factory {}
+ *
+ * const createInstance = () => createSingleton(Factory)
+ *
+ * const ins1 = createInstance()
+ * const ins2 = createInstance()
+ * // ins1 === in2
+ * ```
+ *
+ * @param factory
+ * @returns
+ */
 export function createSingleton<T>(factory: Ctor<T, []> | (() => T)): T {
   if (singletons.has(factory)) {
     return singletons.get(factory) as T
