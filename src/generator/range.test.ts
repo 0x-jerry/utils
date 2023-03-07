d2739a6dd84968fa3e826f2c5aba4f5f636fd8aa
Jerry Wang <x.jerry.wang@gmail.com>
2023-03-07 22:11:07 +0800

    feat!: support one parameter for range

--- src/generator/range.test.ts
+++ src/generator/range.test.ts
@@ -1,17 +1,20 @@
 import { range } from './range'
 
 describe('range', () => {
-  it('should generate 1 to 5', () => {
-    expect(range(1, 6)).eql([1, 2, 3, 4, 5])
+  it('should generate only end', () => {
+    expect(range(2)).eql([0, 1, 2])
+    expect(range(-2)).eql([0, -1, -2])
   })
 
-  it('should generate 10 to 1', () => {
-    expect(range(5, 0)).eql([5, 4, 3, 2, 1])
+  it('should generate with start and end', () => {
+    expect(range(1, 3)).eql([1, 2, 3])
+
+    expect(range(5, 1)).eql([5, 4, 3, 2, 1])
   })
 
   it('should work with step', () => {
     expect(range(1, 10, 2)).eql([1, 3, 5, 7, 9])
     expect(range(0, 5, 2)).eql([0, 2, 4])
-    expect(range(-10, 0, 2)).eql([-10, -8, -6, -4, -2])
+    expect(range(-10, 0, 2)).eql([-10, -8, -6, -4, -2, 0])
   })
 })
--- src/generator/range.ts
+++ src/generator/range.ts
@@ -3,20 +3,28 @@
  * Generate some range, eg. :
  *
  * ```ts
- * range(0, 3) // => [0,1,2]
- * range(3, 0) // => [3,2,1]
+ * range(3) // => [0,1,2,3]
+ * range(-3) // => [0,-1,-2,-3]
+ * range(3, 1) // => [3,2,1]
  * range(0, 5, 2) // => [0,2,4]
  * ```
  *
- * @param end
- * @param start
- * @param step
  * @returns
  */
-export function range(start: number, end: number, step?: number) {
+export function range(end: number): number[]
+export function range(start: number, end: number): number[]
+export function range(start: number, end: number, step: number): number[]
+export function range(start: number, end?: number, step?: number) {
   const nums: number[] = []
 
-  step = step ?? (start > end ? -1 : 1)
+  if (arguments.length === 1) {
+    end = start
+    start = 0
+    step = start > end ? -1 : 1
+  } else {
+    end = end!
+    step = step ?? (start > end ? -1 : 1)
+  }
 
   let num = start
 
@@ -24,12 +32,12 @@ export function range(start: number, end: number, step?: number) {
   let count = 10000
 
   if (start > end) {
-    while (num > end && count--) {
+    while (num >= end && count--) {
       nums.push(num)
       num += step
     }
   } else {
-    while (num < end && count--) {
+    while (num <= end && count--) {
       nums.push(num)
       num += step
     }
