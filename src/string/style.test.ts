e20657a78d3e9f54a16b5d3b29501237f70332ec
Jerry Wang <x.jerry.wang@gmail.com>
2023-03-18 15:23:31 +0800

    feat(string): casing style conversion

---
+++ src/string/style.test.ts
@@ -0,0 +1,27 @@
+import { camelCase, kebab$case, PascalCase, snake_case } from './style'
+
+describe('string style conversion', () => {
+  it('should convert to PascalCase', () => {
+    expect(PascalCase('camelCase')).toBe('CamelCase')
+    expect(PascalCase('snake_case')).toBe('SnakeCase')
+    expect(PascalCase('kebab-case')).toBe('KebabCase')
+  })
+
+  it('should convert to camelCase', () => {
+    expect(camelCase('PascalCase')).toBe('pascalCase')
+    expect(camelCase('snake_case')).toBe('snakeCase')
+    expect(camelCase('kebab-case')).toBe('kebabCase')
+  })
+
+  it('should convert to snake_case', () => {
+    expect(snake_case('PascalCase')).toBe('pascal_case')
+    expect(snake_case('camelCase')).toBe('camel_case')
+    expect(snake_case('kebab-case')).toBe('kebab_case')
+  })
+
+  it('should convert to kebab-case', () => {
+    expect(kebab$case('PascalCase')).toBe('pascal-case')
+    expect(kebab$case('camelCase')).toBe('camel-case')
+    expect(kebab$case('snake_case')).toBe('snake-case')
+  })
+})
---
+++ src/string/style.ts
@@ -0,0 +1,43 @@
+export function PascalCase(str: string) {
+  return (
+    str
+      // snake_case or kebab-case
+      .replace(/[_-]\w/g, (n) => n[1].toUpperCase())
+      // camelCase
+      .replace(/^\w/g, (n) => n.toUpperCase())
+  )
+}
+
+export function camelCase(str: string) {
+  return (
+    str
+      // snake_case or kebab-case
+      .replace(/[_-]\w/g, (n) => n[1].toUpperCase())
+      // PascalCase
+      .replace(/^\w/g, (n) => n.toLowerCase())
+  )
+}
+
+export function snake_case(str: string) {
+  return (
+    str
+      // kebab-case
+      .replace(/\-/g, '_')
+      // PascalCase or camelCase
+      .replace(/(?!^)[A-Z]/g, (n) => '_' + n.toLowerCase())
+      // PascalCase
+      .replace(/^[A-Z]/g, (n) => n.toLowerCase())
+  )
+}
+
+export function kebab$case(str: string) {
+  return (
+    str
+      // snake_case
+      .replace(/_/g, '-')
+      // PascalCase or camelCase
+      .replace(/(?!^)[A-Z]/g, (n) => '-' + n.toLowerCase())
+      // PascalCase
+      .replace(/^[A-Z]/g, (n) => n.toLowerCase())
+  )
+}
