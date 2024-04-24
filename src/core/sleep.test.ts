2fac3d9c2b64110a05774c94abfadc3496f1a64c
0x-jerry <x.jerry.wang@gmail.com>
2024-04-24 15:51:14 +0800

    test: remove unstable unit test

--- src/core/sleep.test.ts
+++ src/core/sleep.test.ts
@@ -1,26 +1,9 @@
 import { sleep } from './sleep'
 
 describe('sleep', () => {
-  it('wait 200 ms', async () => {
-    const test = () => {
-      return new Promise<void>((done) => {
-        let v = 0
-        sleep(200).then(() => (v = 1))
-        expect(v).toBe(0)
-
-        setTimeout(() => {
-          expect(v).toBe(1)
-          done()
-        }, 210)
-      })
-    }
-
-    return test()
-  })
-
   it('cancel sleep', async () => {
     let v = 1
-    const p = sleep(10)
+    const p = sleep()
 
     p.then(() => (v = 2)).catch(() => (v = 3))
 
