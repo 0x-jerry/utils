f491aca0835557b8d0d35a62e29cae0eb4f62e5e
Jerry Wang <x.jerry.wang@gmail.com>
2024-05-08 21:55:38 +0800

    perf: revert deprecate `createPromise`

--- src/core/createPromise.ts
+++ src/core/createPromise.ts
@@ -15,12 +15,6 @@ export enum PromiseStatus {
   Rejected = 'rejected',
 }
 
-/**
- * 
- * @deprecated use `Promise.withResolvers` instead of.
- * 
- * @returns 
- */
 export function createPromise<T>(): PromiseInstance<T> {
   type Resolve = (value: T | PromiseLike<T>) => void
   type Reject = (reason?: any) => void
