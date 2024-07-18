a57dc551f5ce5e95dc427b4c36621cb77f6367d9
Jerry Wang <x.jerry.wang@gmail.com>
2024-07-18 21:05:10 +0800

    perf: test parseURL

--- src/url/parse.test.ts
+++ src/url/parse.test.ts
@@ -13,3 +13,30 @@ describe('parse url', () => {
     expect(t).toBe(false)
   })
 })
+
+describe('parse fallback', () => {
+  let _canParse = URL.canParse
+  beforeAll(() => {
+    // @ts-ignore
+    URL.canParse = undefined
+  })
+  afterAll(() => {
+    URL.canParse = _canParse
+  })
+
+  it('should parse valid url', () => {
+    expect(URL.canParse).toBeUndefined()
+
+    const t = parseURL('/path/url', 'http://example.com')
+
+    expect(t).toBeInstanceOf(URL)
+  })
+
+  it('should return false when parse failed', () => {
+    expect(URL.canParse).toBeUndefined()
+
+    const t = parseURL('/path')
+
+    expect(t).toBe(false)
+  })
+})
