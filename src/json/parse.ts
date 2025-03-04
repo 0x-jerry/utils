ba848816eef9cc63112c4dd857990c24a07f5506
Jerry <x.jerry.wang@gmail.com>
2025-03-04 02:20:17 +0000

    docs: add docs for parseJson

--- src/json/parse.ts
+++ src/json/parse.ts
@@ -1,4 +1,4 @@
-export type TransformJsonData<T> = (data: unknown) => T
+export type TransformJsonData<T> = (data?: unknown) => T
 
 export interface ParesJsonOption<T> {
   transform?: TransformJsonData<T>
@@ -6,12 +6,15 @@ export interface ParesJsonOption<T> {
 
 export function parseJson<T>(jsonContent: string, opt: Required<ParesJsonOption<T>>): T
 export function parseJson<T>(jsonContent: string, opt?: ParesJsonOption<T>): T | undefined
+/**
+ * @returns Undefined if parse failed
+ */
 export function parseJson<T>(jsonContent: string, opt?: ParesJsonOption<T>): T | undefined {
   try {
     const result = JSON.parse(jsonContent)
     return opt?.transform ? opt.transform(result) : result
   } catch (error) {
     // ignore error
-    return opt?.transform?.({})
+    return opt?.transform?.(undefined)
   }
 }
