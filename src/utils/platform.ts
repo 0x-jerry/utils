8bac83e5c4123c3b7970b48639f763419b351d84
Jerry <x.jerry.wang@gmail.com>
2026-01-20 15:23:59 +0800

    fix: treeshake

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,3 +1,11 @@
-export const isWeb = /*@__PURE__*/ (() => !!globalThis.window && !!globalThis.document)()
+function isWebPlatform() {
+  return !!globalThis.window && !!globalThis.document
+}
 
-export const isNode = /*@__PURE__*/ (() => !!globalThis.process?.versions?.node)()
+export const isWeb = /*@__PURE__*/ isWebPlatform()
+
+function isNodePlatform() {
+  return !!globalThis.process?.versions?.node
+}
+
+export const isNode = /*@__PURE__*/ isNodePlatform()
