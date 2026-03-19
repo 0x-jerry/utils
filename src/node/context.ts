648b1257b434dcc2528a1e9ed35595e9dd0cf288
Jerry <x.jerry.wang@gmail.com>
2026-03-19 10:28:01 +0000

    feat: context management utils for node

---
+++ src/node/context.ts
@@ -0,0 +1,82 @@
+import { AsyncLocalStorage } from "node:async_hooks";
+import type { Awaitable } from "../types";
+
+type ASLStore = Map<ContextImplementSymbol, any>;
+
+let als: AsyncLocalStorage<ASLStore> | undefined;
+
+export type ContextImplementSymbol = symbol;
+
+export type ContextImplement<Value = unknown> = [ContextImplementSymbol, Value];
+
+export interface ContextInstance<T> {
+  name: string;
+  get(): T;
+  impl(service: T): ContextImplement<T>;
+}
+
+export function create<Service>(name: string, defaultImpl?: () => Service) {
+  const ContextImpl: ContextInstance<Service> = {
+    name,
+    get() {
+      const v = getAls().getStore();
+
+      if (!v) {
+        throw new Error("Please Use `Context.run` to run the main function");
+      }
+
+      const key = toContextSymbol(ContextImpl);
+
+      if (v.has(key)) {
+        return v.get(key);
+      } else if (defaultImpl) {
+        const value = defaultImpl();
+
+        v.set(key, value);
+        return value;
+      }
+
+      throw new Error(`Implement not found for: ${name}`);
+    },
+    impl(service) {
+      return [toContextSymbol(ContextImpl), service];
+    },
+  };
+
+  return ContextImpl;
+}
+
+export function run(fn: () => Awaitable<void>, serviceImplements?: ContextImplement[]) {
+  const p = Promise.withResolvers<void>();
+
+  const ctx: ASLStore = new Map();
+
+  getAls().run(ctx, async () => {
+    const v = getAls().getStore()!;
+
+    for (const [key, value] of serviceImplements || []) {
+      v.set(key, value);
+    }
+
+    try {
+      await fn();
+      p.resolve();
+    } catch (err) {
+      p.reject(err);
+    }
+  });
+
+  return p.promise;
+}
+
+export function exit(fn: () => Awaitable<void>) {
+  return getAls().exit(() => fn());
+}
+
+function toContextSymbol(v: unknown): ContextImplementSymbol {
+  return v as ContextImplementSymbol;
+}
+
+function getAls() {
+  return (als ??= new AsyncLocalStorage());
+}
