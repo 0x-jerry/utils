693b5d73db7f8f1ce1d45cc088e12a8541708967
Jerry <x.jerry.wang@gmail.com>
2026-03-20 02:57:29 +0000

    chore: use tsdown instead of tsup

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/node/index.ts'],
   format: ['esm'],
+  exports: true,
   sourcemap: true,
-  dts: true,
   clean: true,
 })
