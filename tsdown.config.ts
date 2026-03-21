04d0db5744d365f88fa8cd6ffb191f94805e5b20
Jerry <x.jerry.wang@gmail.com>
2026-03-21 22:57:10 +0800

    fix: build with dts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/node/index.ts'],
   format: ['esm'],
+  dts: true,
   exports: true,
   sourcemap: true,
   clean: true,
